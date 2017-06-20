22cf1c5d188e1bc3e99be05af44ee67a19ba7102
lqys <liqy@reachmed.cn>
2017-06-20 08:43:10 +0800

    第一次提交代码～

---
+++ Runloop/Runloop/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Runloop
+//
+//  Created by cuzZLYues on 2017/6/20.
+//  Copyright © 2017年 cuzZLYues. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
---
+++ Runloop/Runloop/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Runloop
+//
+//  Created by cuzZLYues on 2017/6/20.
+//  Copyright © 2017年 cuzZLYues. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
